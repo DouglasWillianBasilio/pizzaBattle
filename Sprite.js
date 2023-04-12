38dae3254f93371d80cfc6e92825f1c618551dda
Douglas <125305607+DouglasWillianBasilio@users.noreply.github.com>
2023-04-12 15:25:05 -0300

    shadow

--- Sprite.js
+++ Sprite.js
@@ -1,35 +1,47 @@
 class Sprite {
     constructor(config) {
-
-        //set up the image
-        this.image = new Image();
-        this.image.scr = config.src;
-        this.image.onload = () => {
-            this.isLoaded = true;
-        }
-
-        //Configure Animations e initial state
-        this.animations = config.animations || {
-            idleDown: [
-                [0,0]
-            ]
-        }
-        this.currentAnimation = config.currentAnimation || "idleDown";
-        this.currentAnimationFrame = 0;
-
-        //reference the game object 
-        this.gameObject = config.gameObject;
+  
+      //Set up the image
+      this.image = new Image();
+      this.image.src = config.src;
+      this.image.onload = () => {
+        this.isLoaded = true;
+      }
+  
+      //Shadow
+      this.shadow = new Image();
+      this.useShadow = true; //config.useShadow || false
+      if (this.useShadow) {
+        this.shadow.src = "/images/characters/shadow.png";
+      }
+      this.shadow.onload = () => {
+        this.isShadowLoaded = true;
+      }
+  
+      //Configure Animation & Initial State
+      this.animations = config.animations || {
+        idleDown: [
+          [0,0]
+        ]
+      }
+      this.currentAnimation = config.currentAnimation || "idleDown";
+      this.currentAnimationFrame = 0;
+  
+      //Reference the game object
+      this.gameObject = config.gameObject;
     }
-
+  
     draw(ctx) {
-        const x = this.gameObject.x * 16 - 8;
-        const y = this.gameObject.y * 16 - 18;
-
-        this.isLoaded && ctx.drawImage(this.image,
-            0,0,
-            32,32,
-            x,y,
-            32,32,
-            )
+      const x = this.gameObject.x * 16 - 8;
+      const y = this.gameObject.y * 16 - 18;
+  
+      this.isShadowLoaded && ctx.drawImage(this.shadow, x, y);
+  
+      this.isLoaded && ctx.drawImage(this.image,
+        0,0,
+        32,32,
+        x,y,
+        32,32
+      )
     }
-}
+  }
