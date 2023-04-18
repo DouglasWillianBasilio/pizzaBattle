6b99e9f1f68fb57d19e735d9c6175f98e10d3587
Douglas <125305607+DouglasWillianBasilio@users.noreply.github.com>
2023-04-18 19:39:24 -0300

    iniciando o controle pelo teclado
    iniciando o controle pelas teclas.

--- GameObject.js
+++ GameObject.js
@@ -3,12 +3,15 @@ class GameObject {
       // Define as coordenadas iniciais do objeto (por padrão, x=0 e y=0)
       this.x = config.x || 0;
       this.y = config.y || 0;
-  
+      this.direction = config.direction || "down";
       // Cria um novo sprite para o objeto usando as configurações passadas no objeto de configuração
       this.sprite = new Sprite({
         gameObject: this, // passa uma referência para o objeto atual para que o sprite possa acessar suas coordenadas e desenhá-lo corretamente na tela
         src: config.src || "/images/characters/people/hero.png", // define o caminho da imagem a ser usada para o sprite (por padrão, a imagem do herói será usada)
       });
+    }
+    update() {
+
     }
   }
   
